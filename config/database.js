313ecb514a247322d5857fe2c9132227e7ef1498
mseptiawan <mseptiawan017@gmail.com>
2024-11-12 22:49:42 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,8 +1,13 @@
 module.exports = ({ env }) => ({
   connection: {
     client: "mysql",
-    connection: env("DATABASE_URL"),
-    ssl: env.bool("DATABASE_SSL", false),
+    connection: {
+      host: env("DATABASE_HOST"),
+      port: env("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
+      ssl: env.bool("DATABASE_SSL", false),
+    },
   },
-  debug: false,
 });
